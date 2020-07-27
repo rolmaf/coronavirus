9e164e4c150f20e9a898ccf4b4fe5600686ee9d8
rolmaf <webprogrammisthtmlcss@gmail.com>
2020-07-27 16:40:48 +0200

    fix

--- main.js
+++ main.js
@@ -7,67 +7,68 @@ xhr.open("GET", url, true);
 xhr.send();
 
 let response;
-let wrapper = document.querySelector(".cards");
+let cards = document.querySelector(".cards");
 let loading = document.querySelector(".cssload-thecube");
 
 
 xhr.onload = function () {
     response = JSON.parse(xhr.response);
-    wrapper.innerHTML = "";
+    cards.innerHTML = "";
     console.log(response);
     drawCards("ru");
 };
 
 function drawCards(countryCode) {
     let cardTitle;
-    wrapper.innerHTML = "";
-    loading.classList.remove("hidden");
-    wrapper.classList.add("hidden");
+    cards.innerHTML = "";
+    let i = 0;
+    cards.classList.add("hidden");
     if (countryCode === "ru") {
         response.items.map(num => {
-            wrapper.innerHTML += `
-            <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                        <h5 class="card-title">${num.i18nCountryNames.ru}</h5>
-                        <p class="card-text">
-                            <span class="confirmed">Подтвержденных случаев: ${num.confirmed + num.confirmedInc}</span>
-                            <br> 
-                            <span class="recovered">Выздоровело: ${num.recovered + num.recoveredInc}</span>
-                            <br>
-                            <span class="deaths">Смертей: ${num.deaths + num.deathsInc}</span>
-                            <br> 
-                            <span class="confirmedInc"> Сегодня заболело: ${num.confirmedInc}</span>
-                            <br> 
-                            <span class="deathsInc">Сегодня умерло: ${num.deathsInc}</span>
-                        </p>
+            cards.innerHTML += `
+                <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                            <h5 class="card-title">${num.i18nCountryNames.ru}</h5>
+                            <p class="card-text">
+                                <span class="confirmed">Подтвержденных случаев: ${num.confirmed + num.confirmedInc}</span>
+                                <br> 
+                                <span class="recovered">Выздоровело: ${num.recovered + num.recoveredInc}</span>
+                                <br>
+                                <span class="deaths">Смертей: ${num.deaths + num.deathsInc}</span>
+                                <br> 
+                                <span class="confirmedInc"> Сегодня заболело: ${num.confirmedInc}</span>
+                                <br> 
+                                <span class="deathsInc">Сегодня умерло: ${num.deathsInc}</span>
+                            </p>
+                        </div>
                     </div>
-                </div>
-            `;
+                `;
         });
     } else if (countryCode === "en") {
         response.items.map(num => {
-            wrapper.innerHTML += `
-            <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                        <h5 class="card-title">${num.i18nCountryNames.en}</h5>
-                        <p class="card-text">
-                            <span class="confirmed">Confirmed cases: ${num.confirmed + num.confirmedInc}</span>
-                            <br> 
-                            <span class="recovered">Recovered: ${num.recovered + num.recoveredInc}</span>
-                            <br>
-                            <span class="deaths">Deaths: ${num.deaths + num.deathsInc}</span>
-                            <br> 
-                            <span class="confirmedInc">Sick today: ${num.confirmedInc}</span>
-                            <br> 
-                            <span class="deathsInc">Died today: ${num.deathsInc}</span>
-                        </p>
+            cards.innerHTML += `
+                <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                            <h5 class="card-title">${num.i18nCountryNames.en}</h5>
+                            <p class="card-text">
+                                <span class="confirmed">Confirmed cases: ${num.confirmed + num.confirmedInc}</span>
+                                <br> 
+                                <span class="recovered">Recovered: ${num.recovered + num.recoveredInc}</span>
+                                <br>
+                                <span class="deaths">Deaths: ${num.deaths + num.deathsInc}</span>
+                                <br> 
+                                <span class="confirmedInc">Sick today: ${num.confirmedInc}</span>
+                                <br> 
+                                <span class="deathsInc">Died today: ${num.deathsInc}</span>
+                            </p>
+                        </div>
                     </div>
-                </div>
-            `;
+                `;
         });
     };
-    wrapper.classList.remove("hidden");
+    cards.classList.remove("hidden");
     loading.classList.add("hidden");
+
 };
 
 
@@ -88,8 +89,9 @@ function sortCountries() {
 
 
 function myClick(event) {
-    document.querySelector(".hidden").classList.remove("hidden");
-    event.target.classList.add("hidden");
+    loading.classList.remove("hidden");
+    document.querySelector(".hidden-fl").classList.remove("hidden-fl");
+    event.target.classList.add("hidden-fl");
     if (event.target.classList[0] == "russia-flag") {
         drawCards("ru");
     } else if (event.target.classList[0] == "england-flag") {
