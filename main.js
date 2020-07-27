a112d1315a33eca9c0b835beca3003997550d8a9
rolmaf <webprogrammisthtmlcss@gmail.com>
2020-07-27 13:01:45 +0200

    Russian Language

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
-window.onload = () => {
-    let xhr = new XMLHttpRequest();
+let xhr = new XMLHttpRequest();
 
-    let url = "https://covid.2gis.ru/covid19-global.json";
+let url = "https://covid.2gis.ru/covid19-global.json";
 
-    xhr.open("GET", url, true);
+xhr.open("GET", url, true);
 
-    xhr.send();
+xhr.send();
 
-    let width = screen.width;
-    console.log(width);
+let width = screen.width;
+console.log(width);
 
-    // let amountCards = width / (16 * 18);  
-    // amountCards = Math.floor(amountCards);
-    // console.log(amountCards);
+// let amountCards = width / (16 * 18);  
+// amountCards = Math.floor(amountCards);
+// console.log(amountCards);
 
-    // document.querySelector(".search").style.width = `${amountCards * 18}rem`;
+// document.querySelector(".search").style.width = `${amountCards * 18}rem`;
 
 
-    xhr.onload = function () {
-        let response = JSON.parse(xhr.response);
-        let wrapper = document.querySelector(".wrapper");
-        console.log(response);
-        response.items.map(num => {
-            // console.log(num);
-            wrapper.innerHTML += `
+xhr.onload = function () {
+    let response = JSON.parse(xhr.response);
+    let wrapper = document.querySelector(".wrapper");
+    console.log(response);
+    response.items.map(num => {
+        // console.log(num);
+        wrapper.innerHTML += `
             <div class="card" style="width: 18rem;">
                     <div class="card-body">
-                    <h5 class="card-title">${num.countryCode}</h5>
+                    <h5 class="card-title">${num.i18nCountryNames.ru}</h5>
                     <p class="card-text">
                         <span class="confirmed">Подтвержденных случаев: ${num.confirmed + num.confirmedInc}</span>
                         <br> 
@@ -41,16 +40,14 @@ window.onload = () => {
                 </div>
             </div>
             `
-        });
-        // console.log(response);
-    };
+    });
+    // console.log(response);
+};
 
-    console.log(document.querySelector(".search"));
-
-    function sortCountries() {
-        let text = document.querySelector(".search").value;
-        text = text.trim().toLowerCase();
-
+function sortCountries() {
+    let text = document.querySelector(".search").value;
+    text = text.trim().toLowerCase();
+    if (text != "") {
         let cards = document.getElementsByClassName("card-title");
         for (num of cards) {
             if (num.textContent.toLowerCase().indexOf(text) > -1) {
@@ -58,7 +55,7 @@ window.onload = () => {
             } else {
                 num.parentElement.parentElement.style.display = "none";
             };
-        }
-        // console.log(cards);
+        };
     };
+    // console.log(cards);
 };
